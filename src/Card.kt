c37c06cd917db56889fc513c3ff76dd6c26a4e57
matthewtimmons <Matthwebt@yahoo.com>
2018-06-28 20:31:51 -0500

    Initial commit
    Creates classes for Card, Deck, FoundationPile, TableauPile.

---
+++ src/Card.kt
@@ -0,0 +1,9 @@
+val clubs = "Clubs"
+val diamonds = "Diamonds"
+val hearts = "Hearts"
+val spades = "Spades"
+val redSuits = arrayOf(diamonds, hearts)
+val blackSuits = arrayOf(clubs, spades)
+
+data class Card(val value: Int, val suit: String, var faceUp: Boolean = false) {
+}
