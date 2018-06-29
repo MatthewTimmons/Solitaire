c0fcabbad36b432624b3ef26f300c0cc1f17cf88
matthewtimmons <Matthwebt@yahoo.com>
2018-06-28 22:14:27 -0500

    Fixes bugs Adds tests

--- src/Deck.kt
+++ src/Deck.kt
@@ -8,6 +8,7 @@ class Deck {
     fun drawCard(): Card = cardsInDeck.removeAt(0)
     fun reset() {
         cardsInDeck = cards.toMutableList()
+        cardsInDeck.forEach { it.faceUp = false }
         Collections.shuffle(cardsInDeck)
     }
 
